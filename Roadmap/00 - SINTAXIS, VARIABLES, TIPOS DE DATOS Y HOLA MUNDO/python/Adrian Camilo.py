c9b300b0766e25972dd53268e136cd65fa4691b5
Adrian Camilo <adriancamilojimenezacevedo272@gmail.com>
2026-04-06 19:48:44 -0500

    Cambios

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Adrian Camilo.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Adrian Camilo.py
@@ -1,1 +1,27 @@
 # https://www.python.org/
+# Comentario 
+"""
+
+Varias lineas
+
+"""
+
+'''
+Otro comentario 
+
+'''
+#Variables
+
+My_Variable = "Myvar"
+My_Variable= "Myvar2"
+
+My_Constante = "No Cambia"
+
+My_int = 1
+My_Float = 1.5
+My_Bool = True
+My_Bool= False
+my_string = "hola"
+my_other_string = "holax2"
+
+print ( "Adrian vamos avanzando ")
